6bcf6eaf20048e3965cce14e8f0dd008f4a8a580
hbrashid <hbrashid@gmail.com>
2020-06-10 21:03:31 -0500

    specs completed

--- src/components/Car.js
+++ src/components/Car.js
@@ -2,10 +2,23 @@ import React from 'react'
 import cars from '../cars.json'
 // import material ui components here //
 // Container, Paper, Chip //
+import { Container, Paper, Chip } from "@material-ui/core"
 
 const Car = (props) => {
+    const id = props.match.params.id
+    const car = cars.find(car => car.id == id)
     return (
-        <h1>A specific car</h1>
+        <Container maxWidth="sm">
+            <Paper>
+                <h1>{car.Name}</h1>
+                {Object.keys(car).map((key) => { return <Chip label={`${key}: ${car[key]}`}></Chip>
+                })}
+
+            </Paper>
+
+
+
+        </Container>
     )
 }
 
